7b034fb791ebb0dcc328c071f3549809fc398b9e
Michele <michele.giacomelli@gmail.com>
2017-08-01 22:50:56 +0200

    games

--- src/entity/decorator.js
+++ src/entity/decorator.js
@@ -1,6 +1,6 @@
 // @flow
-import { innerEntityObject } from "../internal/singleton";
-import { buildDefinitions } from "../swagger/definitions";
+import {innerEntityObject} from "../internal/singleton";
+import {buildDefinitions} from "../swagger/definitions";
 
 /**
  * Description 生成实体类的唯一标识，这里首先默认使用实体类名作为唯一标识
@@ -9,7 +9,7 @@ import { buildDefinitions } from "../swagger/definitions";
  * @param descriptor
  */
 export function generateEntityUUID(target, key, descriptor) {
-  return target.constructor.name;
+    return target.constructor.name;
 }
 
 /**
@@ -18,82 +18,80 @@ export function generateEntityUUID(target, key, descriptor) {
  * @returns {*}
  */
 export function entity(Class) {
-  new Class();
-  return Class;
+    new Class();
+    return Class;
 }
 
 /**
  * Description 创建某个属性的描述
  * @param type 基础类型 self - 表示为自身
+ * @param format
+ * @param readOnly
  * @param description 描述
  * @param required 是否为必要参数
  * @param defaultValue 默认值
  * @param pattern
  * @param primaryKey 是否为主键
+ * @param example
+ *
  * @returns {Function}
  */
 export function entityProperty({
-  // 生成接口文档需要的参数
-  type = "string",
-  description = "",
-  required = false,
-  defaultValue = undefined,
+                                   type = "string",
+                                   format = "string",
+                                   readOnly = false,
+                                   description = "",
+                                   required = false,
+                                   defaultValue = undefined,
+                                   pattern = undefined,
+                                   primaryKey = false,
+                                   example = undefined
+                               }) {
+    return function (target, key, descriptor) {
+        let entityUUID = generateEntityUUID(target, key, descriptor);
 
-  // 进行校验所需要的参数
-  pattern = undefined,
+        // 确保实体键存在
+        _ensure(entityUUID, key);
 
-  // 进行数据库连接需要的参数
-  primaryKey = false,
-  example = undefined
-}) {
-  return function(target, key, descriptor) {
-    let entityUUID = generateEntityUUID(target, key, descriptor);
+        let valueObject = innerEntityObject[entityUUID]["properties"][key];
 
-    // 确保实体键存在
-    _ensure(entityUUID, key);
+        // 判断是否为自身
+        if (type === "self" || type === undefined) {
+            valueObject.type = target.constructor;
+        } else if (
+            Array.isArray(type) &&
+            (type[0] === "self" || type[0] === undefined)
+        ) {
+            valueObject.type = [target.constructor];
+        } else {
+            valueObject.type = type;
 
-    let valueObject = innerEntityObject[entityUUID]["properties"][key];
+            // 这里动态编译关联的定义项目，需要根据输入的是否为数组来动态提取出具体的实体类或者对象
+            Array.isArray(type) ? buildDefinitions(type[0]) : buildDefinitions(type);
+        }
 
-    // 判断是否为自身
-    if (type === "self" || type === undefined) {
-      valueObject.type = target.constructor;
-    } else if (
-      Array.isArray(type) &&
-      (type[0] === "self" || type[0] === undefined)
-    ) {
-      valueObject.type = [target.constructor];
-    } else {
-      valueObject.type = type;
+        valueObject.description = description;
 
-      // 这里动态编译关联的定义项目，需要根据输入的是否为数组来动态提取出具体的实体类或者对象
-      Array.isArray(type) ? buildDefinitions(type[0]) : buildDefinitions(type);
-    }
+        if (required) {
+            innerEntityObject[entityUUID]["required"].push(key);
+            valueObject.allowNull = false;
+        } else {
+            valueObject.allowNull = true;
+        }
 
-    // 设置描述
-    valueObject.description = description;
+        valueObject.pattern = pattern;
+        valueObject.defaultValue = defaultValue;
+        valueObject.primaryKey = primaryKey;
+        valueObject.example = example;
+        valueObject.readOnly = readOnly;
+        if (!target.constructor.attrTypes){
+            target.constructor.attrTypes = {}
+        }
+        target.constructor.attrTypes[key] = valueObject;
+        descriptor.writable = true;
 
-    // 如果是必须属性，则添加到列表中
-    if (required) {
-      innerEntityObject[entityUUID]["required"].push(key);
-
-      // 对应地不允许为空
-      valueObject.allowNull = false;
-    } else {
-      // 允许为空
-      valueObject.allowNull = true;
-    }
-
-    // 设置其他属性
-    valueObject.pattern = pattern;
-    valueObject.defaultValue = defaultValue;
-    valueObject.primaryKey = primaryKey;
-    valueObject.example = example;
-
-    // 这里需要设置下 writeable 为 true
-    descriptor.writable = true;
-
-    return descriptor;
-  };
+        return descriptor;
+    };
 }
 
 /**
@@ -103,14 +101,14 @@ export function entityProperty({
  * @private
  */
 function _ensure(entityUUID, property) {
-  innerEntityObject[entityUUID] || (innerEntityObject[entityUUID] = {});
+    innerEntityObject[entityUUID] || (innerEntityObject[entityUUID] = {});
 
-  innerEntityObject[entityUUID]["required"] ||
+    innerEntityObject[entityUUID]["required"] ||
     (innerEntityObject[entityUUID]["required"] = []);
 
-  innerEntityObject[entityUUID]["properties"] ||
+    innerEntityObject[entityUUID]["properties"] ||
     (innerEntityObject[entityUUID]["properties"] = {});
 
-  innerEntityObject[entityUUID]["properties"][property] ||
+    innerEntityObject[entityUUID]["properties"][property] ||
     (innerEntityObject[entityUUID]["properties"][property] = {});
 }
